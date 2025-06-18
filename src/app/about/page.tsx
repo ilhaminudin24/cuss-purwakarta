d2e2ed797d365642017e9013ebdbc76fae14c5df
Ilhmndn <ilhaminudin24@gmail.com>
2025-06-18 17:52:26 +0700

    fix: add readonly prop to MapField component type definition

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ export default function AboutPage() {
     <section className="max-w-2xl mx-auto py-12 px-4 text-center">
       <h2 className="text-2xl sm:text-3xl font-bold text-orange-500 mb-4">Tentang CUSS Purwakarta</h2>
       <p className="text-sm text-black/80 mb-4">
-      CUSS Purwakarta adalah layanan jasa antar suruh terpercaya di Purwakarta, yang siap membantu antar makanan dan barang, antar-jemput anak sekolah, titip belanja atau beli, bayar tagihan dan ambil antrian, ambil laundry atau barang tertinggal, serta kirim dokumen, paket, stok, dll.
+      CUSS Purwakarta hadir sebagai jasa antar suruh di Purwakarta yang bertujuan memudahkan aktivitas sehari-hari warga Purwakarta. Kami memahami bahwa waktu dan kenyamanan adalah hal berharga, sehingga hadir dengan solusi layanan cepat, aman, dan fleksibel sesuai kebutuhan.
       </p>
     </section>
   );
